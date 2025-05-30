db914cb3a89d909addf23ab18496b40b6a6ebe45
limj0009 <limj0009@gmail.com>
2025-05-30 10:18:30 -0500

    fix bugs

--- src/components/outputSpeech.js
+++ src/components/outputSpeech.js
@@ -3,12 +3,6 @@ export const outputSpeech = (text) => {
     return;
   }
   const speech = new SpeechSynthesisUtterance(text);
-  const voices = window.speechSynthesis.getVoices();
-  const samantha = voices.find(voice => voice.name === "Samantha");
-
-  if (samantha) {
-    speech.voice = samantha;
-  }
   speech.lang = 'en-US'; 
   speech.rate = 2;
   speech.pitch = 1;
